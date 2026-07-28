d92231f41b12b707bf4a39378d3b33badbdc0795
Shayana Shrestha <shayana0324@gmail.com>
2026-07-28 14:00:08 -0400

    Body component

--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Body } from './body/body';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Body],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('first-app');
+  title = ('First-app-assignment');
 }
