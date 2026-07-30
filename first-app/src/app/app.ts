0866e5c6f8eed846627962bb900da5f3c749460b
Shayana Shrestha <shayana0324@gmail.com>
2026-07-30 09:18:30 -0400

    Data binding

--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Body } from './body/body';
+import { DataBinding } from './data-binding/data-binding';
 import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header,Body, Footer],
+  imports: [RouterOutlet, Header, Body, DataBinding, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- first-app/src/app/data-binding/data-binding.ts
+++ first-app/src/app/data-binding/data-binding.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+  // Interpolation Binding
+  location = 'Forsyth County, GA';
+  meetingTime = '6:30 PM';
+  currentBook = 'The Midnight Library by Matt Haig';
+
+  // Property Binding
+  isDisabled = true;
+
+  // Event Binding
+  showMessage() {
+    alert("Next book: Atomic Habits by James Clear");
+  }
+}
